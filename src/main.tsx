1bf76d0d0008eb65c922e161fa31950eab948c64
Marco Klein <marco.klein@trustedshops.de>
2023-06-19 23:57:23 +0200

    build: :sparkles: Add SCSS support

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { render } from 'solid-js/web';
 import { PresentationView } from './views/presentation/presentation.view';
 
+import './main.scss';
+
 const app = document.getElementById('app');
 
 if (app) {
