7ccdec0d348eb2ba88c842a336916bb72187b398
Marco Klein <marco.klein@trustedshops.de>
2023-06-20 23:28:35 +0200

    build(core): :sparkles: Add asset copying to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
+
 import solidPlugin from 'vite-plugin-solid';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    viteStaticCopy({
+      targets: [{
+        src: path.resolve(__dirname, './src/assets') + '/[!.]*', // 1️⃣
+        dest: './assets', // 2️⃣
+      }]
+    }),
+  ],
 });
