76873e150b9b9ccf413d2ef8fb127e01c52f4e3e
Hobart2967 <hobart2967@outlook.com>
2025-03-22 23:01:18 +0100

    refactor: 🚚 Move Hobby project to its own component

---
+++ src/components/gwen/gwen.tsx
@@ -0,0 +1,23 @@
+export function Gwen() {
+  return <div>
+    <div class="title">Hobby Project - Game Engineering</div>
+    <div class="card hobby-project gwen">
+      <p>
+        My passion for software development was basically originating from the will to create games. Because I discarded the thought of starting a career as a game developer before my studies, I decided to make it a hobby
+      </p>
+      <p>
+        This is why and how my first game engineering project started. Currently, it's called GWEN (Game without extra-ordinary name). It is developed using the awesome Godot Game Engine, and shall become an MMORPG, based on inspiration by games like World of Warcraft, Lord of the Rings Online, ARK, Minecraft and some others that I used to play in my spare time.
+
+        The project itself is work in progress, screenshots and material may follow.
+      </p>
+      <p>
+        In parallel, I try to build a Godot-dedicated library based on common software design patterns that have become established over the last years.
+      </p>
+      <div class="screenshots">
+        <img src="/assets/images/gwen/gwen1.png" />
+        <img src="/assets/images/gwen/gwen2.png" />
+        <img src="/assets/images/gwen/gwen3.png" />
+      </div>
+    </div>
+  </div>;
+}
